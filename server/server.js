3214fedd88ed467865e3fdbe9f3d04618cdc45d5
yosetdn <yosetdn@gmail.com>
2019-01-08 16:26:45 -0600

    Add patch /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -51,6 +52,27 @@ app.delete('/todos/:id',(req,res)=>{
    }).catch((e)=>{
      res.status(400).send(e);
    })
+});
+
+app.patch('/todos/:id',(req,res)=>{
+    const id = req.params.id;
+    let body = _.pick(req.body,['text','completed']);
+    if(!ObjectID.isValid(id)) return res.status(404).send('');
+
+    if(_.isBoolean(body.completed)&&body.completed){
+        body.completedAt = new Date().getTime();
+    }else{
+      body.completed = false;
+      body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id,{$set: body},{new: true}).then((todo)=>{
+      if(!todo) return res.status(404).send();
+      res.send({todo});
+    }).catch((e)=>{
+      res.status(400).send();
+    })
+
 });
 
 app.listen(3000,()=>{
