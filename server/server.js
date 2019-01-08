cf0c4b2c1e3ba313cb370a74a9dc6ff535e4311f
yosetdn <yosetdn@gmail.com>
2019-01-08 16:09:43 -0600

    Add DELETE todo

--- server/server.js
+++ server/server.js
@@ -42,6 +42,17 @@ app.get('/todos/:id',(req,res)=>{
    })
 });
 
+app.delete('/todos/:id',(req,res)=>{
+  const id = req.params.id;
+  if(!ObjectID.isValid(id)) return res.status(404).send('');
+  Todo.findByIdAndRemove(id).then((todo)=>{
+     if(!todo) return res.status(404).send('Your item doesnt exist');
+     res.send(todo);
+   }).catch((e)=>{
+     res.status(400).send(e);
+   })
+});
+
 app.listen(3000,()=>{
   console.log('Started on port 3000');
 });
